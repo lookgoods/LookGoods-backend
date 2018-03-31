c04c025d93f73c1d23983218866f7e0bf7e9e405
gunhappy <phasin.s@ku.th>
2018-04-01 02:05:48 +0700

    can upload and resize image to gcloud

--- src/api/controllers/uploadController.js
+++ src/api/controllers/uploadController.js
@@ -1,5 +1,6 @@
 import storage from '@google-cloud/storage'
 import DotEnv from 'dotenv'
+import sharp from 'sharp'
 
 DotEnv.config()
 
@@ -15,33 +16,78 @@ function getPublicUrl (filename) {
 }
 
 export function sendUploadToGCS (req, res, next) {
-	console.log(req.file, 'file')
 	if (!req.file) {
 		res.status(400).send('No file uploaded.')
 		return
 	}
 
-	const gcsname = Date.now() + req.file.originalname
-	const file = bucket.file(gcsname)
+	const originalName = `original/${Date.now()}-${req.file.originalname}`
+	const originalFile = bucket.file(originalName)
 
-	const stream = file.createWriteStream({
+	var picture = {
+		picture_url: null,
+		picture_thumbnail_url: null
+	}
+
+	const stream = originalFile.createWriteStream({
 		metadata: {
 			contentType: req.file.mimetype
 		}
 	})
 
 	stream.on('error', (err) => {
-		req.file.cloudStorageError = err
 		res.status(400).send(err)
 	})
 
 	stream.on('finish', () => {
-		req.file.cloudStorageObject = gcsname
-		file.makePublic().then(() => {
-			req.file.cloudStoragePublicUrl = getPublicUrl(gcsname)
-			res.status(200).send(req.file.cloudStoragePublicUrl)
+		originalFile.makePublic().then(() => {
+			const cloudStoragePublicUrl = getPublicUrl(originalName)
+			picture.picture_url = cloudStoragePublicUrl
+
+			const thumbnailName = `thumbnail/thumbnail-${Date.now()}-${req.file.originalname}`
+			const thumbnailFile = bucket.file(thumbnailName)
+
+			sharp(req.file.buffer).resize(360, 360).ignoreAspectRatio().toBuffer().then((data) => {
+				const stream = thumbnailFile.createWriteStream({
+					metadata: {
+						contentType: req.file.mimetype
+					}
+				})
+				stream.on('error', (err) => {
+					res.status(400).send(err)
+				})
+				stream.on('finish', () => {
+					thumbnailFile.makePublic().then(() => {
+						const cloudStoragePublicUrl = getPublicUrl(thumbnailName)
+						picture.picture_thumbnail_url = cloudStoragePublicUrl
+
+						res.status(200).send(picture)
+					})
+				})
+
+				stream.end(data)
+			})
 		})
 	})
 
 	stream.end(req.file.buffer)
+
+	sharp(req.file.buffer).resize(360, 360).toBuffer().then((data) => {
+		const stream = originalFile.createWriteStream({
+			metadata: {
+				contentType: req.file.mimetype
+			}
+		})
+		stream.on('error', (err) => {
+			res.status(400).send(err)
+		})
+		stream.on('finish', () => {
+			originalFile.makePublic().then(() => {
+				const cloudStoragePublicUrl = getPublicUrl(originalName)
+				res.status(200).send(cloudStoragePublicUrl)
+			})
+		})
+
+		stream.end(data)
+	})
 }
