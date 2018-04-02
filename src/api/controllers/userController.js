dfb68eb2d24d60b40cbc2a9ff6ad88c8d42fee7d
jidapar <jidapar.j@ku.th>
2018-04-02 23:18:09 +0700

    using populate in userController

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -2,11 +2,6 @@ import Mongoose from 'mongoose'
 import User from '../models/userModel'
 
 export default {
-    getUserList: (req, res) => User.find({}, (err, user) => {
-        if (err) res.send(err)
-        res.json(user)
-    }),
-
     createUser: (req,res) => {
         const newUser = new User(req.body)
         newUser.save((err, user) => {
@@ -15,24 +10,44 @@ export default {
         })
     },
 
-    getUser: (req, res) => User.find({_id:req.params.id}, (err, user) => {
-        if (err) res.send(err)
-        res.json(user[0])
+    getUserList: (req, res) => User.find({})
+        .populate('own_post_list')
+        .exec( (err, user) => {
+            if (err) res.send(err)
+            res.json(user)
     }),
 
-    editUserInfo: (req, res) => User.update({_id:req.session.user_id}, {
-        description: req.body.description
-    }, (err, user) => {
-        if (err) res.send(err)
-        res.send(user)
+    getUser: (req, res) => User.find({ _id:req.params.id })
+        .populate('follower_list')
+        .populate('following_list')
+        .populate('saved_post_list')
+        .populate('own_post_list')
+        .exec( (err, user) => {
+            if (err) res.send(err)
+            res.json(user[0])
     }),
 
-    getCurrentUser: (req, res) => User.find({_id:req.session.user_id}, (err,currentUser) =>{
-        if (err) res.send(err)
-        res.json(currentUser[0])
+    editUserInfo: (req, res) => User.update(
+        {
+            _id:req.session.user_id
+        }, {
+            description: req.body.description
+        }, (err, user) => {
+            if (err) res.send(err)
+            res.send(user)
     }),
 
-    getFollower: (req, res) => User.find({_id:req.params.id} ,(err, user) => {
+    getCurrentUser: (req, res) => User.find({ _id:req.session.user_id })
+        .populate('follower_list')
+        .populate('following_list')
+        .populate('saved_post_list')
+        .populate('own_post_list')
+        .exec( (err,currentUser) =>{
+            if (err) res.send(err)
+            res.json(currentUser[0])
+    }),
+
+    getFollower: (req, res) => User.find({ _id:req.params.id } ,(err, user) => {
         if (err) res.send(err)
         User.find({_id:{ $in: user[0].follower_list}}, (err, user_list) => {
             if (err) res.send(err)
@@ -40,7 +55,7 @@ export default {
         })
     }),
 
-    getFollowing: (req, res) => User.find({_id:req.params.id} ,(err, user) => {
+    getFollowing: (req, res) => User.find({ _id:req.params.id } ,(err, user) => {
         if (err) res.send(err)
         User.find({_id:{ $in: user[0].following_list}}, (err, user_list) => {
             if (err) res.send(err)
@@ -48,42 +63,66 @@ export default {
         })
     }),
 
-    followUser: (req, res) => User.find({_id:req.session.user_id, following_list:req.params.id}, (err,currentUser) => {
-        if (err) res.send(err)
-        if (currentUser.length == 0){
-            User.update({_id:req.params.id}, {
-                $push: {follower_list: req.session.user_id}
-            }, (err, updateFollower) => {
-                if (err) res.send(err)
-                User.update({_id:req.session.user_id}, {
-                    $push: {following_list: req.params.id}
-                }, (err, updateFollowing) => {
-                    if (err) res.send(err)
-                    res.send(updateFollowing)
-                })
-            })
-        }else{
-            res.json(currentUser)
-        }
+    followUser: (req, res) => User.find(
+        {
+            _id:req.session.user_id, 
+            following_list:req.params.id
+        }, (err, currentUser) => {
+            if (err) res.send(err)
+            if (currentUser.length == 0){
+                User.update(
+                    {
+                        _id:req.params.id
+                    }, {
+                        $push: { follower_list: req.session.user_id }
+                    }, (err, updateFollower) => {
+                        if (err) res.send(err)
+                        User.update(
+                            {
+                                _id:req.session.user_id
+                            }, {
+                                $push: { following_list: req.params.id }
+                            }, (err, updateFollowing) => {
+                                if (err) res.send(err)
+                                res.send(updateFollowing)
+                            }
+                        )
+                    }
+                )
+            }else{
+                res.json(currentUser)
+            }
     }),
 
-    unfollowUser: (req, res) => User.find({_id:req.session.user_id, following_list:req.params.id}, (err,currentUser) => {
-        if (err) res.send(err)
-        if (currentUser.length == 0){
-            res.json(currentUser)
-        }else{
-            User.update({_id:req.params.id}, {
-                $pull: {follower_list: req.session.user_id}
-            }, (err, updateFollower) => {
-                if (err) res.send(err)
-                User.update({_id:req.session.user_id}, {
-                    $pull: {following_list: req.params.id}
-                }, (err, updateFollowing) => {
-                    if (err) res.sond(err)
-                    res.send(updateFollowing)
-                })
-            })
-        }
+    unfollowUser: (req, res) => User.find(
+        {
+            _id:req.session.user_id, 
+            following_list:req.params.id
+        }, (err, currentUser) => {
+            if (err) res.send(err)
+            if (currentUser.length == 0){
+                res.json(currentUser)
+            }else{
+                User.update(
+                    {
+                        _id:req.params.id
+                    }, {
+                        $pull: { follower_list: req.session.user_id }
+                    }, (err, updateFollower) => {
+                        if (err) res.send(err)
+                        User.update(
+                            {
+                                _id:req.session.user_id
+                            }, {
+                                $pull: {following_list: req.params.id}
+                            }, (err, updateFollowing) => {
+                                if (err) res.sond(err)
+                                res.send(updateFollowing)
+                            }
+                        )
+                    }
+                )
+            }
     })
     
     
